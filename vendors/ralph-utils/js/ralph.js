8faeb16da6a7a5a3b7cde541cb2a534fa0fc3b22
ralphxp <raphapanchi@gmail.com>
2024-01-22 04:01:33 +0100

    new file:   images/business_phjr7u.png new file:   images/data_lqqahn.png modified:   index.html new file:   test.html new file:   vendors/ralph-utils/css/ralph.css new file:   vendors/ralph-utils/js/ralph.js

---
+++ vendors/ralph-utils/js/ralph.js
@@ -0,0 +1,9 @@
+
+document.querySelector('.r-flip-card')?
+document
+    .querySelector('.r-flip-card')
+        .addEventListener('click', function() {
+            this.
+                classList
+                    .toggle('is-flipped');
+                }):0;
